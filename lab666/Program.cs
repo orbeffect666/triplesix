931a5635b688694333b5b685e52e8ee74ff2f337
orbeffect666 <akhmetgaleev1996@mail.ru>
2021-12-27 22:21:58 +0300

    triplesix

---
+++ lab666/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace Lab3sem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[10];
+            FileStream fs = new FileStream("FileStreamTest.txt",
+            FileMode.OpenOrCreate);
+            if (fs.Length == 0)
+            {
+                Console.WriteLine("Writing Data...");
+                for (short i = 0; i < 10; i++)
+                    data[i] = (byte)i;
+                fs.Write(data, 0, 10); // Запись данных
+            }
+            else
+            {
+                fs.Seek(-5, SeekOrigin.End); // Ищем конец
+                int count = fs.Read(data, 0, 10); // Чтение данных
+                for (int i = 0; i < count; i++) Console.WriteLine(data[i]);
+            }
+            fs.Close();
+        }
+    }
+}
